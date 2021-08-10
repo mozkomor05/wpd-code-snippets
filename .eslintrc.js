2d6a56098ffa9543c8b1efbd3e33bb94e3315cf4
David Moškoř <mozkomor05@gmail.com>
2021-08-10 09:44:25 +0200

    code format & small bugs

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    rules: {
+        "comma-dangle": 1,
+        "no-cond-assign": 1,
+        "no-console": 1,
+        "no-constant-condition": 1,
+        "no-control-regex": 1,
+        "no-debugger": 1,
+        "no-dupe-args": 1,
+        "no-dupe-keys": 1,
+        "no-duplicate-case": 1,
+        "no-empty-character-class": 1,
+        "no-empty": 1,
+        "no-ex-assign": 1,
+        "no-extra-boolean-cast": 1,
+        "no-extra-semi": 1,
+        "no-func-assign": 1,
+        "no-inner-declarations": 1,
+        "no-invalid-regexp": 1,
+        "no-irregular-whitespace": 1,
+        "no-negated-in-lhs": 1,
+        "no-obj-calls": 1,
+        "no-regex-spaces": 1,
+        "no-sparse-arrays": 1,
+        "no-unreachable": 1,
+        "use-isnan": 1,
+        "valid-typeof": 1,
+        "no-fallthrough": 1,
+        "no-octal": 1,
+        "no-redeclare": 1,
+        "no-delete-var": 1,
+        "no-undef": 1,
+        "no-unused-vars": 1,
+        "no-mixed-spaces-and-tabs": 1,
+    }
+};
